1db9c4c966fbfb4fbaf5351a2e723eedd14c674a
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-09 19:58:58 +0000

    Create Contact.vue

---
+++ src/pages/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      This the Contact component
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
