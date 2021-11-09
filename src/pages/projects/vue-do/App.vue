766b21421933974bdb079e4c9552016204e265c1
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-09 19:58:46 +0000

    Create App.vue

---
+++ src/pages/projects/vue-do/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <TodoList />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/compomemts/TodoList'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+</style>
