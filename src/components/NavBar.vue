8798c6816414c65f4da93968171605ea4646cf54
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-09 19:57:43 +0000

    Create NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <router-link :to="{name: 'home'}">Home</router-link> |
+      <router-link :to="{name: 'about'}">About</router-link> |
+      <router-link :to="{name: 'projects'}">My Projects</router-link> |
+      <router-link :to="{name: 'contact'}">Contact</router-link> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style>
+</style>
