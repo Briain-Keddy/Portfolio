2c7b77f429deb1c46a4c6fd8f80516b69a41de16
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-09 19:58:51 +0000

    Create AllProjects.vue

---
+++ src/pages/projects/AllProjects.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      This the All Project component
+      <div v-for="Project in projects"
+      :key="Project.id">
+        Title: {{ Project.title }}<br>
+        Description: {{ Project.description}}<br>
+        <router-link :to="{name: project.demo}">Demo</router-link> 
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return {
+      projects: []
+    }
+  },
+  mounted(){
+    this.getAllProjects()
+  },
+  methods: {
+    getAllProjects(){
+      fetch('./data/projects.json')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.projects= data
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
