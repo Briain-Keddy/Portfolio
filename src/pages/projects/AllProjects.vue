dc77e091ce7f1bd62079b6a4e51f8422911d0273
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-17 22:24:23 +0000

    Update AllProjects.vue

--- src/pages/projects/AllProjects.vue
+++ src/pages/projects/AllProjects.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="columns">
     <div class="column">
-      <b-field label=""></b-field>
+      <b-field label="Search Projects">
+        <b-input v-model="searchTerm"></b-input>
+      </b-field>
       <div class="card"
         v-for="project in filterProjects"
         :key="project.id">
-      >
         <div class="card-image">
           <figure class="image is-4by3">
             <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
@@ -13,21 +14,24 @@
         </div>
         <div class="card-content">
           <div class="media">
-            <div class="media-left">
+            <!-- <div class="media-left">
               <figure class="image is-48x48">
                 <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
               </figure>
-            </div>
+            </div> -->
             <div class="media-content">
               <p class="title is-4">{{ project.title }}</p>
               <p class="subtitle is-6">
                 <b-taglist>
-                  <b-tag v-for="tag in project.tag" :key="tag" type="is-primary"></b-tag>
+                  <b-tag 
+                    v-for="tag in project.tags" 
+                    :key="tag" 
+                    type="is-primary"
+                  >
+                    {{ tag }}
+                  </b-tag>
                 </b-taglist>
               </p>
-              <b-field v-for="tag in project.tags" :key="tag">
-                <b-tag>{{ tag }}</b-tag>
-              </b-field>
             </div>
           </div>
           <div class="content">
@@ -35,19 +39,6 @@
           </div>
         </div>
       </div>
-    </div>
-    <input type="text" v-model="searchTerm" />
-    <div>
-      <b-card 
-        header="project"
-        header-text-varient="white"
-        header-bg-varient="dark"
-        v-for="project in filterProjects"
-        :key="project.id">
-          Title: {{ project.title }}<br>
-          Description: {{ project.description}}<br>
-          <router-link v-if="project.demo" :to="{name: project.demo}">Demo</router-link>
-      </b-card>
     </div>
   </div>
 </template>
