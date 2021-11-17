8ae90dd5422b454b0fd08e0eed7b56ebf93ae852
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2021-11-17 17:59:35 +0000

    Update AllProjects.vue

--- src/pages/projects/AllProjects.vue
+++ src/pages/projects/AllProjects.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    <input type="text" v-model="searchTerm" /><br><br>
+  <div class="columns">
+    <div class="column">
+      <b-field></b-field>
+      <div class="card"
+        v-for="project in filterProjects"
+        :key="project.id">
+      >
+        <div class="card-image">
+          <figure class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+          </figure>
+        </div>
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left">
+              <figure class="image is-48x48">
+                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-4">John Smith</p>
+              <p class="subtitle is-6">@johnsmith</p>
+              <b-field v-for="tag in project.tags" :key="tag">
+                <b-tag>{{ tag }}</b-tag>
+              </b-field>
+            </div>
+          </div>
+          <div class="content">
+            {{ project.description }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <input type="text" v-model="searchTerm" />
     <div>
       <b-card 
         header="project"
@@ -22,6 +54,7 @@ export default {
   name: 'App',
   components: {
   },
+
   data(){
     return {
       projects: [],
