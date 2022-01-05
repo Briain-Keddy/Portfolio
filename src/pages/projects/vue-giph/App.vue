9981f776934a573d854cdc57ce5ab6f01d5c9269
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2022-01-05 17:32:42 +0000

    Update App.vue

--- src/pages/projects/vue-giph/App.vue
+++ src/pages/projects/vue-giph/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <b-col>
+      <b-row>
+        this is the app
+        <GiphyViewer />
+      </b-row>
+    </b-col>
   </div>
 </template>
 
 <script>
+import GiphyViewer from '@/pages/projects/GiphyViewer.vue'
 
 export default {
   name: 'App',
   components: {
+    GiphyViewer
   }
 }
 </script>
