b6ff69d60b029fa044a9b6e59691eddae23bc783
Briain Keddy <71636045+Briain-Keddy@users.noreply.github.com>
2022-01-05 17:32:36 +0000

    Update GiphyViewer.vue

--- src/pages/projects/vue-giph/GiphyViewer.vue
+++ src/pages/projects/vue-giph/GiphyViewer.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
+    <div class="search-box">
+      <input type="text" v-model="term" v-on:keyup.enter="searchGiphy()" />
+      <b-button class="float-right" variant="primary" @click="searchGiphy()">Search</b-button>
+    </div>
+  
+  <b-card-group columns>
+    <b-card v-for="gif in gifs" 
+      :key="gif.id" 
+      :img-src="gif.images.fixed_width.url" 
+      :img-alt="git.title"
+    >
+    <b-card-text>
+      <a :href="gif.url" target="_blank">{{ gif.title }}</a>
+    </b-card-text>
+    </b-card>
+  </b-card-group>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const GIPHY_URL = "https://api.giphy.com/v1/gifs";
+const API_KEY = "MlNSOIaRLVqElstMdH9uDT94U8QKg5kb";
 
 export default {
   name: 'GiphyViewer',
@@ -11,12 +31,32 @@ export default {
   },
   data(){
       return{
+        gifs: []
       };
   },
+  mouunted(){
+    axios.get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
+         .then(respone =>(this.gifs = respone.data.data))
+         .catch(error => console.log(error))
+  },
   methods:{
+    searchGiphy(){
+      if(!this.term){
+        alert("Please enter a search term");
+        return;
+      }
+      axios.get(`${GIPHY_URL}/search?api_key=${API_KEY}&limit=20`)
+         .then(respone =>(this.gifs = respone.data.data))
+         .catch(error => console.log(error))
+      this.term = "";
+    }
   }
 }
 </script>
 
 <style>
+.search-box{
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 </style>
